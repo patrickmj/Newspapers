6290f8436a354afa895d4788bb08a9ee3aa88ecf
patrickmj <patrickmjchnm@gmail.com>
2016-05-06 21:31:04 -0400

    newspaper statistics sql query refactoring with extreme prejudice

--- models/Table/NewspapersNewspaper.php
+++ models/Table/NewspapersNewspaper.php
@@ -31,18 +31,18 @@ class Table_NewspapersNewspaper extends Omeka_Db_Table
             $states = $params['states'];
         }
         
-        $select = $this->getSelect();
-        $newspaperId = 1;
+        $select = new Omeka_Db_Select($this->getDb()->getAdapter());
         
         $db = $this->_db;
+        
+        $select->from($db->NewspapersNewspaper, '*');
+        /*
         $select->from($db->NewspapersNewspaper,
                     new Zend_Db_Expr("max({$db->NewspapersFrontPage}.page_width) as maxPageWidth"));
         $select->from($db->NewspapersNewspaper,
                     new Zend_Db_Expr("min({$db->NewspapersFrontPage}.page_width) as minPageWidth"));
         $select->from($db->NewspapersNewspaper,
                     new Zend_Db_Expr("avg({$db->NewspapersFrontPage}.page_width) as avgPageWidth"));
-        
-        
         $select->from($db->NewspapersNewspaper,
                     new Zend_Db_Expr("std({$db->NewspapersFrontPage}.page_width) as stdPageWidth"));
         
@@ -52,8 +52,6 @@ class Table_NewspapersNewspaper extends Omeka_Db_Table
                     new Zend_Db_Expr("min({$db->NewspapersFrontPage}.printspace_width) as minPrintSpaceWidth"));
         $select->from($db->NewspapersNewspaper,
                     new Zend_Db_Expr("avg({$db->NewspapersFrontPage}.printspace_width) as avgPrintSpaceWidth"));
-        
-        
         $select->from($db->NewspapersNewspaper,
                     new Zend_Db_Expr("std({$db->NewspapersFrontPage}.printspace_width) as stdPrintSpaceWidth"));
 
@@ -64,9 +62,8 @@ class Table_NewspapersNewspaper extends Omeka_Db_Table
                     new Zend_Db_Expr("min({$db->NewspapersFrontPage}.page_height) as minPageHeight"));
         $select->from($db->NewspapersNewspaper,
                     new Zend_Db_Expr("avg({$db->NewspapersFrontPage}.page_height) as avgPageHeight"));
-        
         $select->from($db->NewspapersNewspaper,
-                    new Zend_Db_Expr("std({$db->NewspapersFrontPage}.printspace_height) as stdPageHeight"));
+                    new Zend_Db_Expr("std({$db->NewspapersFrontPage}.page_height) as stdPageHeight"));
         
         
         $select->from($db->NewspapersNewspaper,
@@ -75,10 +72,8 @@ class Table_NewspapersNewspaper extends Omeka_Db_Table
                     new Zend_Db_Expr("min({$db->NewspapersFrontPage}.printspace_height) as minPrintSpaceHeight"));
         $select->from($db->NewspapersNewspaper,
                     new Zend_Db_Expr("avg({$db->NewspapersFrontPage}.printspace_height) as avgPrintSpaceHeight"));
-
-        
         $select->from($db->NewspapersNewspaper,
-                    new Zend_Db_Expr("std({$db->NewspapersFrontPage}.printspace_width) as stdPrintSpaceHeight"));
+                    new Zend_Db_Expr("std({$db->NewspapersFrontPage}.printspace_height) as stdPrintSpaceHeight"));
 
         
         $select->from($db->NewspapersNewspaper,
@@ -88,26 +83,57 @@ class Table_NewspapersNewspaper extends Omeka_Db_Table
         $select->from($db->NewspapersNewspaper,
                     new Zend_Db_Expr("avg({$db->NewspapersFrontPage}.columns) as avgColumns"));
         
+        */
+        /*
         $select->from($db->NewspapersNewspaper,
                     new Zend_Db_Expr("std({$db->NewspapersFrontPage}.columns) as stdColumns"));
-        
+        */
         
         
         $select->join($db->NewspapersIssue,
                 "{$db->NewspapersIssue}.newspaper_id = {$db->NewspapersNewspaper}.id", array());
 
-             
-
-
-        
-        
         $select->join($db->NewspapersFrontPage, 
-                "{$db->NewspapersFrontPage}.issue_id = {$db->NewspapersIssue}.id", array());
+                "{$db->NewspapersFrontPage}.issue_id = {$db->NewspapersIssue}.id", 
+                array(
+                      new Zend_Db_Expr("std({$db->NewspapersFrontPage}.columns) as stdColumns"),
+                      new Zend_Db_Expr("avg({$db->NewspapersFrontPage}.columns) as avgColumns"),
+                      new Zend_Db_Expr("min({$db->NewspapersFrontPage}.columns) as minColumns"),
+                      new Zend_Db_Expr("max({$db->NewspapersFrontPage}.columns) as maxColumns"),
         
-        $select->where("{$db->NewspapersNewspaper}.state IN (?)", $states);
-        $select->where("{$db->NewspapersNewspaper}.id IN (?)", $newspaperIds);
+                      new Zend_Db_Expr("max({$db->NewspapersFrontPage}.printspace_height) as maxPrintSpaceHeight"),
+                      new Zend_Db_Expr("min({$db->NewspapersFrontPage}.printspace_height) as minPrintSpaceHeight"),
+                      new Zend_Db_Expr("avg({$db->NewspapersFrontPage}.printspace_height) as avgPrintSpaceHeight"),
+                      new Zend_Db_Expr("std({$db->NewspapersFrontPage}.printspace_height) as stdPrintSpaceHeight"),
+                      
+                      new Zend_Db_Expr("max({$db->NewspapersFrontPage}.page_height) as maxPageHeight"),
+                      new Zend_Db_Expr("min({$db->NewspapersFrontPage}.page_height) as minPageHeight"),
+                      new Zend_Db_Expr("avg({$db->NewspapersFrontPage}.page_height) as avgPageHeight"),
+                      new Zend_Db_Expr("std({$db->NewspapersFrontPage}.page_height) as stdPageHeight"),
+                      
+                      
+                      new Zend_Db_Expr("max({$db->NewspapersFrontPage}.page_width) as maxPageWidth"),
+                      new Zend_Db_Expr("min({$db->NewspapersFrontPage}.page_width) as minPageWidth"),
+                      new Zend_Db_Expr("avg({$db->NewspapersFrontPage}.page_width) as avgPageWidth"),
+                      new Zend_Db_Expr("std({$db->NewspapersFrontPage}.page_width) as stdPageWidth"),
         
-echo $select;
+                      new Zend_Db_Expr("max({$db->NewspapersFrontPage}.printspace_width) as maxPrintSpaceWidth"),
+                      new Zend_Db_Expr("min({$db->NewspapersFrontPage}.printspace_width) as minPrintSpaceWidth"),
+                      new Zend_Db_Expr("avg({$db->NewspapersFrontPage}.printspace_width) as avgPrintSpaceWidth"),
+                      new Zend_Db_Expr("std({$db->NewspapersFrontPage}.printspace_width) as stdPrintSpaceWidth"),
+                      
+                ));
+        
+        if (! empty($states)) {
+            $select->where("{$db->NewspapersNewspaper}.state IN (?)", $states);
+        }
+        
+        if (! empty($newspaperIds)) {
+            $select->where("{$db->NewspapersNewspaper}.id IN (?)", $newspaperIds);
+        }
+        // $select->where("{$db->NewspapersNewspaper}.id = ?", $newspaperIds);
+        
+debug($select);
 $result = $this->_db->fetchAll($select);
 print_r($result);
 
